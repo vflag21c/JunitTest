67a8e69fa6cabc7b352f272658b833dbbede648b
유열 <vflag21c@nate.com>
2024-02-19 23:37:50 +0900

    junit 학습 6일차( archunit ) commit

--- src/test/java/org/example/junittest/acrhunittest/OnionArchitecture.java
+++ src/test/java/org/example/junittest/acrhunittest/OnionArchitecture.java
@@ -12,9 +12,19 @@ public class OnionArchitecture {
     /**
      * 다양한 아키텍처 검증이 가능하다.
      * 참고 : https://www.archunit.org/userguide/html/000_Index.html#_architectures
+     *     : https://d2.naver.com/helloworld/9222129 ( 네이버 정산프로세스 개발자 )
      * example source : https://github.com/TNG/ArchUnit-Examples/blob/main/example-plain/src/test/java/com/tngtech/archunit/exampletest/OnionArchitectureTest.java
      */
 
+    /**
+     *  - 패키지 의존성 체크
+     *  - 클래스 의존성 체크
+     *  - 패키지 네이밍 룰 체크
+     *  - 구현체 네이밍 룰 체크
+     *  - 구현체 어노테이션 체크
+     *  - 레이어 체크
+     *  - 순환구조 체크
+     */
 
     //어니언(헥사고날) 아키텍처 검증.
     @Test
